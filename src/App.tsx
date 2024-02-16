aa5be004518cad06cd220cb0d499692caf4f9ca7
Manuel Cota <cota.manuel@rocketmail.com>
2024-02-15 22:40:20 -0700

    Connected RT to Backcaliria

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import LockPage from './components/lockPage';
 import InsidePage from './components/insidePage';
 import backgroundTexture from './images/background_texture.jpg';
+import { useEffectOnce } from 'react-use';
+
+
 
 function App() {
+
+  const [d1, setD1] = useState<number>(0)
+  const [d2, setD2] = useState<number>(0)
+  const [d3, setD3] = useState<number>(0)
+  const [d4, setD4] = useState<number>(0)
+  const [d5, setD5] = useState<number>(0)
+  const [clave, setClave] = useState<string>("")
+  const [mensaje, setMensaje] = useState<string>("")
+
+
+  useEffectOnce(() => {
+    get_RT_sol()
+  })
+
+  const get_RT_sol = async () => {
+      await fetch('https://backcaliria.vercel.app/rtsol')
+      .then(res => res.json())
+      .then(sol => {
+          setD1(sol.digit1)
+          setD2(sol.digit2)
+          setD3(sol.digit3)
+          setD4(sol.digit4)
+          setD5(sol.digit5)
+          setClave(sol.clave)
+          setMensaje(sol.message)
+
+      });
+      console.log("Se hizo un GET")
+  }
+
   return (
     <div
       style={{
@@ -21,15 +54,15 @@ function App() {
     }}>
       <Routes>
         <Route path='/' element={<LockPage
-          digit1={1}
-          digit2={1}
-          digit3={1}
-          digit4={1}
-          digit5={1}
+          digit1={d1}
+          digit2={d2}
+          digit3={d3}
+          digit4={d4}
+          digit5={d5}
         />} />
         <Route path='/ze' element={<InsidePage 
-          msg={"ABRIR ZONA DE ENTRENAMIENTO"}
-          clave={"CO'ORPHIK"}
+          msg={mensaje}
+          clave={clave}
         />} />
       </Routes>
     </div>
